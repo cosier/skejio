928a9bbe035b9d6c9ec40129b78d9fb0feb1c1b2
Bailey Cosier <bailey.cosier@gmail.com>
2014-11-17 11:25:32 +0700

    [tests] adding controller specs for ScheduleRulesController create action

---
+++ spec/controllers/schedule_rules_controller_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+describe ScheduleRulesController, :type => :controller do
+
+  before :each do
+  end
+
+  context '#create' do
+
+    before do
+      @user = create(:admin)
+      sign_in(@user)
+
+      @post_data = {
+        business_id: @user.business.id,
+        schedule_rule: {
+          service_provider_id: @user.id,
+          sheets: [
+            {
+              valid_from: "Now",
+              valid_until: "Forever",
+              services: nil,
+              entries: gen_entries(count: 15)
+            }
+          ],
+          breaks: gen_entries(count: 15)
+        }
+      }
+
+      post :create, @post_data
+    end
+
+
+    it 'does not blow up' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'saves 15 BreakShifts onto the ScheduleRule' do
+      expect(assigns(:schedule_rule).break_shifts.length).to eq(15)
+    end
+
+    it 'saves 15 TimeEntries onto the ScheduleRule' do
+      expect(assigns(:schedule_rule).time_entries.length).to eq(15)
+    end
+
+    it 'doesn\'t set validity dates when using natural text' do
+      expect(assigns(:schedule_rule).time_sheets.first.valid_from_at).to eq(nil)
+      expect(assigns(:schedule_rule).time_sheets.first.valid_until_at).to eq(nil)
+    end
+
+    it 'saves the ScheduleRule' do
+      expect(assigns(:schedule_rule)).to be_persisted
+    end
+
+  end #create
+
+
+
+end
