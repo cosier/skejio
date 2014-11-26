a558f4eb32dec3864d90c94e2af647a75b1eb4cb
Bailey Cosier <bailey.cosier@gmail.com>
2014-11-26 14:08:51 +0700

    [wip] Adding class static loader for Session and Customer models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,27 @@
+# == Schema Information
+#
+# Table name: customers
+#
+#  id                :integer          not null, primary key
+#  phone_number      :string(255)
+#  first_name        :string(255)
+#  last_name         :string(255)
+#  sms_verified      :boolean          default(FALSE)
+#  voice_verified    :boolean          default(FALSE)
+#  verification_code :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class Customer < ActiveRecord::Base
   has_many :system_logs
   has_many :sessions
+  
+  validates_uniqueness_of :phone_number
+
+  class << self
+    def load(number)
+      Customer.first_or_create(phone_number: number)
+    end
+  end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,2 +1,21 @@
+# == Schema Information
+#
+# Table name: sessions
+#
+#  id          :integer          not null, primary key
+#  business_id :integer
+#  customer_id :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Session < ActiveRecord::Base
+
+  validates_uniqueness_of :customer_id, :scope => :business_id
+
+  class << self
+    def load(customer, business)
+      Session.first_or_create(customer_id: customer.id, business_id: business.id)
+    end
+  end
 end
