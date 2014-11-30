411867d80db2e3385514e85bc82d0076873139a9
Bailey Cosier <bailey.cosier@gmail.com>
2014-12-01 04:13:57 +0700

    [feature] Implemented Office and Services selection interfaces for both Voice and SMS

--- lib/skej/state_logic/office_selection.rb
+++ lib/skej/state_logic/office_selection.rb
@@ -4,6 +4,15 @@ module Skej
 
       def think
 
+        @offices = @session.business.available_offices.to_a
+        @offices_ordered = {}
+        @human_readable_offices = []
+
+        @offices.each_with_index do |office, index|
+          @offices_ordered[index + 1] = office
+          @human_readable_offices << "#{index + 1} - #{office.name}"
+        end
+
         # Automatic pass, not enough offices to choose from.
         if @session.business.available_offices.length < 2
           log "Available Offices < 2 <br/><strong>Skipping Customer Selection of Offices</strong>"
@@ -11,6 +20,44 @@ module Skej
           return advance!
         end
 
+        @digits = @session.input[:Digits] || strip_to_int(@session.input[:Body])
+
+        # Early bail out if already completed
+        if get[:office_selection] and get[:office_selection].to_sym == :complete
+          log "Office selection already complete"
+          advance!
+        end
+
+        # Attempt processing of the digits
+        if @digits.present?
+          log "Processing Customer Input Digits: <strong>#{@digits}</strong>"
+
+          if @chosen_office = @offices_ordered[@digits.to_i]
+            log "Customer has Selected Office: <strong>#{@chosen_office.display_name}</strong>"
+
+            get[:office_selection] = :complete
+            get[:chosen_office_id] = @chosen_office.id
+
+            # Since we utilized the input, we must clear
+            clear_session_input!
+            advance!
+          else
+            @bad_selection = true
+            log "Available Offices: <br/>#{@human_readable_offices.to_json}"
+            log "Oops, selection(#{@digits}) was not matched to any available Office"
+          end
+        end
+
+      end
+
+      def sms_and_voice
+        if @bad_selection and @digits.present?
+          # Show a sorry, incorrect input page and ask again
+          twiml_repeat_office_selection offices: @offices_ordered
+        else
+          # Just render normal menu selection
+          twiml_ask_office_selection offices: @offices_ordered
+        end
       end
 
     end
---
+++ lib/skej/twiml/repeat_office_selection.rb
@@ -0,0 +1,43 @@
+module Skej
+  module Twiml
+    class RepeatOfficeSelection < BaseTwiml
+
+      def sms(session)
+        build_twiml do |b|
+          b.Message "Sorry we did not recognize the option \"#{session.input[:Body]}\" "
+
+          office_choices = ""
+          options[:offices].map do |index, office|
+            office_choices << "Enter #{index} for #{office.name} \n"
+          end
+
+          b.Message office_choices
+
+        end
+      end
+
+      def voice(session)
+        build_twiml do |b|
+          b.Gather action: endpoint(gathering: true), maxlength: 10, timeout: 120,  finishOnKey: "#", method: 'get' do |g|
+
+            g.Pause length: 1
+            g.Say "Please select a Office for your Appointment"
+            g.Pause length: 1
+            g.Say "Followed by the pound key to continue"
+            g.Pause length: 2
+
+            options[:offices].map do |index, office|
+              g.Say "Enter #{index} for #{office.name} ...."
+            end
+
+            g.Pause length: 1
+            g.Say "Press the pound key to listen to the available Offices again"
+          end
+        end
+      end
+
+    end
+  end
+end
+
+
