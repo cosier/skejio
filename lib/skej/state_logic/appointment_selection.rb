575ad42f5d96ffb847bf8aae2507a4dad22c0fa9
Bailey Cosier <bailey.cosier@gmail.com>
2014-11-30 19:16:46 +0700

    [up] adding provider and appointment logic module stubs

---
+++ lib/skej/state_logic/appointment_selection.rb
@@ -0,0 +1,11 @@
+module Skej
+  module StateLogic
+    class AppointmentSelection < BaseLogic
+
+      def think
+      end
+
+    end
+  end
+end
+
---
+++ lib/skej/state_logic/provider_selection.rb
@@ -0,0 +1,17 @@
+module Skej
+  module StateLogic
+    class ProviderSelection < BaseLogic
+
+      def think
+      end
+
+      def sms
+      end
+
+      def voice
+      end
+
+    end
+  end
+end
+
