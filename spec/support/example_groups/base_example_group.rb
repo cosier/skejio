2b2903d356d18c62f69c01861d1a66e9d9b95c93
Bailey Cosier <bailey.cosier@gmail.com>
2015-01-29 00:45:23 +0700

    tests: updating base_example_group to include sign_in helpers

--- spec/support/example_groups/base_example_group.rb
+++ spec/support/example_groups/base_example_group.rb
@@ -1,6 +1,11 @@
+require 'devise/test_helpers'
+
 module BaseExampleGroup
+  # Module extensions
   extend ::ActiveSupport::Concern
   extend ::RSpec::ExampleGroups
 
+  # Method includes
+  include ::Devise::TestHelpers
   include ::Rack::Test::Methods
 end
